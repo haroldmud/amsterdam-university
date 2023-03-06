4a75ffb736e899dc9e221b8d275c9f4af5e85953
haroldmud <haroldmudosa@gmail.com>
2023-03-06 18:21:15 +0200

    Feat: Add functionality to the footer

--- components/interface/footer.jsx
+++ components/interface/footer.jsx
@@ -1,8 +1,15 @@
 import { SlArrowUp } from 'react-icons/sl'
+import { SlArrowDown } from 'react-icons/sl'
 import { SlArrowRight } from 'react-icons/sl'
 import { FaFacebookF, FaTwitter } from 'react-icons/fa'
+import { useState } from 'react'
+
 
 export default function Footer(){
+  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen1, setIsOpen1] = useState(false)
+  const [isOpen2, setIsOpen2] = useState(false)
+  const [isOpen3, setIsOpen3] = useState(false)
   return (
     <section className="bg-dark mt-20">
       <section className="max-w-[80rem] text-white mx-auto w-full">
@@ -11,45 +18,73 @@ export default function Footer(){
           <a href="#header" className="w-fit h-fit p-3 bg-gray-400 hover:bg-grey hover:text-dark rounded-sm"><SlArrowUp/></a>
         </div>
         <div className="flex lg:flex-row flex-col  pl-2 lg:gap-32 gap-6 mt-12">
-          <div className="flex-col flex gap-2">
-            <h2 className="font-bold mb-2">Education & research</h2>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Bachelor's programmes</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+          <div className="flex-col flex gap-2 lg:border-none border-b border-gray-400 mr-2">
+            <h2 className="font-bold pb-3 lg:block hidden lg:my-0 my-auto ">Education & research</h2>
+            <div onClick={()=> setIsOpen(current => !current)} className='lg:hidden flex  justify-between lg:border-none border-t  border-gray-400 py-4'>
+              <h2 className="font-bold mb-2 lg:my-0 my-auto">Education & research</h2>
+              <span className='lg:hidden my-auto text-[0.7rem] pr-4'>
+                { isOpen ? <SlArrowUp/> :<SlArrowDown/>}</span>
+            </div>
+            <div className={`${isOpen ? "block pb-3" : "hidden"}`}>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Bachelor's programmes</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+            </div>
           </div>
-          <div className="flex-col flex gap-2">
-            <h2 className="font-bold mb-2">Go to</h2>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Bachelor's programmes</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+          <div className="flex-col flex gap-2 lg:border-none border-b border-gray-400 mr-2">
+            <h2 className="font-bold pb-3 lg:block hidden lg:my-0 my-auto ">Go to</h2>
+            <div onClick={()=> setIsOpen1(current => !current)} className='lg:hidden flex  justify-between lg:border-none border-t  border-gray-400 py-4'>
+              <h2 className="font-bold mb-2 lg:my-0 my-auto">Go to</h2>
+              <span className='lg:hidden my-auto text-[0.7rem] pr-4'>
+                { isOpen1 ? <SlArrowUp/> :<SlArrowDown/>}</span>
+            </div>
+            <div className={`${isOpen1 ? "block pb-3" : "hidden"}`}>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Bachelor's programmes</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+            </div>
           </div>
-          <div className="flex-col flex gap-2">
-            <h2 className="font-bold mb-2">Information for</h2>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> BWebmail for UvA staff</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+          <div className="flex-col flex gap-2 lg:border-none border-b border-gray-400 mr-2">
+            <h2 className="font-bold pb-3 lg:block hidden lg:my-0 my-auto ">Information for</h2>
+            <div onClick={()=> setIsOpen2(current => !current)} className='lg:hidden flex  justify-between lg:border-none border-t  border-gray-400 py-4'>
+              <h2 className="font-bold mb-2 lg:my-0 my-auto">Information for</h2>
+              <span className='lg:hidden my-auto text-[0.7rem] pr-4'>
+                { isOpen2 ? <SlArrowUp/> :<SlArrowDown/>}</span>
+            </div>
+            <div className={`${isOpen2 ? "block pb-3" : "hidden"}`}>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> BWebmail for UvA staff</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Minors</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Master's programmes</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Professional development</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Exchange</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Summer School</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> PhD at UvA</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Research at UvA</span>
+            </div>
           </div>
-          <div className="flex-col flex gap-2">
-            <h2 className="font-bold mb-2">Contact</h2>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Contact information</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Locations</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Contact Student Services</span>
-            <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> The UvA and social media</span>
+          <div className="flex-col flex gap-2 lg:border-none border-b border-gray-400 mr-2">
+            <h2 className="font-bold pb-3 lg:block hidden lg:my-0 my-auto ">Contact</h2>
+            <div onClick={()=> setIsOpen3(current => !current)} className='lg:hidden flex  justify-between lg:border-none border-t  border-gray-400 py-4'>
+              <h2 className="font-bold mb-2 lg:my-0 my-auto">Information for</h2>
+              <span className='lg:hidden my-auto text-[0.7rem] pr-4'>
+                { isOpen3 ? <SlArrowUp/> :<SlArrowDown/>}</span>
+            </div>
+            <div className={`${isOpen3 ? "block pb-3" : "hidden"}`}>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Contact information</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Locations</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> Contact Student Services</span>
+              <span className="flex gap-1 text-[0.8rem] font-semibold"><span className="text-[0.5rem] my-auto "><SlArrowRight/></span> The UvA and social media</span>
+            </div>
           </div>
         </div>
         <div className=" border-t border-gray-400 lg:flex hidden justify-between px-2 text-gray-400 py-4 mt-8">
